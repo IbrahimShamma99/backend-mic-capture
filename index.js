f00cafa5ba377da844b0a2f8bb0dabfefeb11fcb
ibrahimshamma99 <i.abushammah@gmail.com>
2022-05-18 01:28:32 +0300

    updates

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-let Mic = require('./Mic');
+let Mic = require("./Mic");
 let mic = new Mic();
-const fs = require("fs")
+const fs = require("fs");
+var player = require("play-sound")((opts = {}));
+
 let micStream = mic.startRecording();
 
-var myWritableStream = fs.WriteStream('output.raw');
+var myWritableStream = fs.WriteStream("output.raw");
 
-
-micStream.pipe( myWritableStream );
-mic.on('info', (info) => {
-	console.log(info);
+micStream.pipe(myWritableStream);
+mic.on("info", (info) => {
+  console.log(info);
 });
-mic.on('error', (error) => {
-	console.log(error);
+mic.on("error", (error) => {
+  console.log(error);
 });
+const audio = "masa_ward.mp3";
 
- setTimeout(() => {
-     mic.stopRecording();
+player.play(audio, function (err) {
+    if (err) throw err;
+  });
+
+
+setTimeout(() => {
+  try {
+    mic.stopRecording();
+  } catch (e) {}
+
+  // $ mplayer foo.mp3
+
+  // access the node child_process in case you need to kill it on demand
 }, 10000);
